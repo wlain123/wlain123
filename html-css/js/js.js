a0c4156449b154619a8e6acc13fa1de75e8ead17
wlain123 <139905994+wlain123@users.noreply.github.com>
2023-07-18 19:58:36 +0300

    Add files via upload

---
+++ html-css/js/js.js
@@ -0,0 +1,13 @@
+const name = 1;
+
+const boom = 2;
+
+const bee = 3;
+
+console.log(name);
+
+console.log(boom);
+
+console.log(bee);
+
+console.log("Спасибо за то что ты сюда посмотрел и прости за баги поставь масштаб на 100% чтобы исправить их")
